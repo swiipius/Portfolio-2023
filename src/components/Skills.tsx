9b533ec6e25460b65363e08acd593554d44a3e58
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-10-09 19:59:55 -0400

    update images and add projects

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -20,31 +20,31 @@ function Skills({}: Props) {
         </h3>
 
         <div className='grid grid-cols-6 gap-5 mx-5'>
-            <Skill level='80%' logo='/images/Logo/arduino.png' />
-            <Skill level='65%' logo='/images/Logo/c.png' />
-            <Skill level='45%' logo='/images/Logo/c++.png' />
-            <Skill level='70%' logo='/images/Logo/css.svg' />
-            <Skill level='20%' logo='/images/Logo/docker.png' />
-            <Skill level='80%' logo='/images/Logo/esp32.jpg' />
-            <Skill level='70%' logo='/images/Logo/figma.png' />
-            <Skill level='80%' logo='/images/Logo/fusion.png' />
-            <Skill level='30%' logo='/images/Logo/gazebo.png' />
-            <Skill level='90%' logo='/images/Logo/github.png' />
-            <Skill level='70%' logo='/images/Logo/hass.png' />
+            <Skill level='90%' logo='/images/Logo/vs.svg' />
+            <Skill level='90%' logo='/images/Logo/shortcut.svg' />
+            <Skill level='90%' logo='/images/Logo/github.svg' />
             <Skill level='90%' logo='/images/Logo/homekit.png' />
-            <Skill level='65%' logo='/images/Logo/html.png' />
+            <Skill level='80%' logo='/images/Logo/arduino.svg' />
+            <Skill level='80%' logo='/images/Logo/esp32.svg' />
+            <Skill level='80%' logo='/images/Logo/python.svg' />
+            <Skill level='80%' logo='/images/Logo/raspberry.svg' />
+            <Skill level='80%' logo='/images/Logo/fusion.png' />
+            <Skill level='70%' logo='/images/Logo/hass.svg' />
             <Skill level='70%' logo='/images/Logo/kicad.png' />
-            <Skill level='65%' logo='/images/Logo/linux.png' />
-            <Skill level='10%' logo='/images/Logo/matter_bg.png' />
-            <Skill level='50%' logo='/images/Logo/mqtt.png' />
-            <Skill level='30%' logo='/images/Logo/php.png' />
-            <Skill level='80%' logo='/images/Logo/pi.png' />
-            <Skill level='80%' logo='/images/Logo/python.png' />
-            <Skill level='60%' logo='/images/Logo/React.png' />
-            <Skill level='70%' logo='/images/Logo/ros2.png' />
-            <Skill level='90%' logo='/images/Logo/shortcut.png' />
-            <Skill level='10%' logo='/images/Logo/thread.png' />
-            <Skill level='90%' logo='/images/Logo/vs.png' />
+            <Skill level='70%' logo='/images/Logo/ros2.png'  css='bg-white rounded-xl object-contain' />
+            <Skill level='70%' logo='/images/Logo/css.svg' />
+            <Skill level='70%' logo='/images/Logo/figma.svg' />
+            <Skill level='65%' logo='/images/Logo/c.svg' />
+            <Skill level='65%' logo='/images/Logo/html.svg' />
+            <Skill level='65%' logo='/images/Logo/linux.svg' />
+            <Skill level='60%' logo='/images/Logo/react.svg' />
+            <Skill level='50%' logo='/images/Logo/mqtt.svg' />
+            <Skill level='10%' logo='/images/Logo/thread.svg' />
+            <Skill level='45%' logo='/images/Logo/c++.svg' />
+            <Skill level='30%' logo='/images/Logo/gazebo.png' />
+            <Skill level='30%' logo='/images/Logo/php.svg' />
+            <Skill level='20%' logo='/images/Logo/docker.svg' />
+            <Skill level='10%' logo='/images/Logo/matter.svg' css='bg-white rounded-xl object-contain'/> 
         </div>
     </motion.div>
   )
